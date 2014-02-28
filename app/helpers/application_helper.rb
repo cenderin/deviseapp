7017b5862b7498b887960068f4b94416491756bb
cenderin <ml.alexander87@gmail.com>
2014-02-27 16:56:32 -0800

    devise and stuff

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,8 @@
+module ApplicationHelper
+	def gravatar_url(email, options = {})
+		digest = Digest::MD5.hexdigest(email)
+		url_options = {host: "www.gravatar.com/avatar/#{digest}", d: "retro"}.merge(options)
+		url = url_for(url_options)
+		url
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Facebase::Application.config.secret_key_base = 'b11c8b6f74f6f50825d352ff2f0acaf8345b08c1a5de425744d45252b8bc414d94c55faa4372d0f6dc8a71d52dd339367aad9a60a2121f91e1a62ef857132139'
