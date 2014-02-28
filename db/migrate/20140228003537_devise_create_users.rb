6baa9631263e08cdb00008bc49ac10d69aea41bc
cenderin <ml.alexander87@gmail.com>
2014-02-28 15:13:33 -0800

    seeds

--- db/migrate/20140228003537_devise_create_users.rb
+++ db/migrate/20140228003537_devise_create_users.rb
@@ -5,14 +5,11 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
-
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
-
       ## Rememberable
       t.datetime :remember_created_at
-
       ## Trackable
       t.integer  :sign_in_count, :default => 0, :null => false
       t.datetime :current_sign_in_at
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,6 @@
 		email: Faker::Internet.email,
 		password: 'password',
 		password_confirmation: 'password'
-
-
-
-
-
-
+		
 		})
 end
